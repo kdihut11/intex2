af500d509776bb9b49e42a8f25422e7abc22b0b9
Tanner Hunt <tannerhunt@Tanners-MacBook-Pro.local>
2020-04-08 10:08:13 -0600

    updates to left container

--- gofundmereact/src/left-container.js
+++ gofundmereact/src/left-container.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import * as bs from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
+import AppContext from './context'
 
 function LeftContainer(props) {
+    const context = React.useContext(AppContext)
+    const campaigns = context.campaigns
+    const sortedDateCampaigns = campaigns.slice().sort((a, b) => b.launch_date - a.launch_date)
+    const sortedGoalCampaigns = campaigns.slice().sort((a,b) => b.goal - a.goal)
+
     return (
         <div>
-        <bs.Nav className="flex-column">
-            <NavLink to="/" className="nav-link nav-link-ltr" style={{ color: 'black' }}>Categories to Search by</NavLink>
-        </bs.Nav>
-  </div>
+            <h2 className='text-center'>
+                Categories
+            </h2>
+            <bs.Nav className="flex-column">
+                <NavLink key={sortedDateCampaigns.launch_date} to={'/sort/date'} className="nav-link nav-link-ltr" style={{ color: 'black' }}>Sort By Campaign Date</NavLink>
+                <NavLink key={sortedGoalCampaigns.goal} to={'/sort/goal'} className="nav-link nav-link-ltr" style={{ color: 'black' }}>Sort By Donation Goal</NavLink>
+            </bs.Nav>
+        </div>
     )  
 }
 

b46de38dd814bff2b71686c95791fad96edffce1
Keara Hutchinson <kearahutchinson@Kearas-MacBook-Pro.local>
2020-04-09 12:06:35 -0600

    changes to product details page

--- gofundmereact/src/Welcome.js
+++ gofundmereact/src/Welcome.js
@@ -1,11 +1,11 @@
-//import React, { useState } from "react";
-import React from 'react'
-//import * as b from "bootstrap";
-//import GoogleLogin from "react-google-login";
-//import FacebookLogin from "react-facebook-login";
+import React, { useState } from "react";
+// import React from 'react'
+import * as b from "bootstrap";
+import GoogleLogin from "react-google-login";
+import FacebookLogin from "react-facebook-login";
 import App from "./App.js";
-//import goFundMe from "./goFundMe.png";
-//import background from "./background.jpeg";
+import goFundMe from "./goFundMe.png";
+import background from "./background.jpeg";
 
 function Welcome(props) {
   // var sectionStyle = {
@@ -49,58 +49,58 @@ function Welcome(props) {
 
   
 
-//   if (!name && !FName) {
-//     return (
-//       <div>
-//         <div style={sectionStyle}>
-//           <div
-//             style={{
-//               backgroundColor: "rgba(0, 0, 0, 0.4)",
-//               height: "100%",
-//               width: "100%",
-//             }}
-//           >
-//             <div className="p-5" style={{ width: 600, color: "white" }}>
-//               <img
-//                 alt=""
-//                 src={goFundMe}
-//                 width="300"
+  // if (!name && !FName) {
+  //   return (
+  //     <div>
+  //       <div style={sectionStyle}>
+  //         <div
+  //           style={{
+  //             backgroundColor: "rgba(0, 0, 0, 0.4)",
+  //             height: "100%",
+  //             width: "100%",
+  //           }}
+  //         >
+  //           <div className="p-5" style={{ width: 600, color: "white" }}>
+  //             <img
+  //               alt=""
+  //               src={goFundMe}
+  //               width="300"
                 
-//               />
-//               <h1 className="p-2">Welcome to Go Fund Me Analytics!</h1>{" "}
-//               <br></br>
-//               <h3 className="p-2">
-//                 Please log in with your Google or Facebook account below to continue
-//               </h3>
-//               <br></br>
-//               <table>
-//                 <tr>
-//                   <td className='p-2'>
-//                     <GoogleLogin
-//                       clientId="264235252416-qu7uoto6sb62kc8hb7fffrg387o26e7e.apps.googleusercontent.com"
-//                       buttonText="Login with Google"
-//                       onSuccess={responseGoogle}
-//                       onFailure={responseGoogle}
-//                       cookiePolicy={"single_host_origin"}
-//                       className="p-2"
-//                     />
-//                   </td>
-//                   <td className='p-2'>
-//                     <FacebookLogin
-//                       appId="2498350440413934"
-//                       autoLoad={true}
-//                       fields="name,email,picture"
-//                       callback={responseFacebook}
-//                     />
-//                   </td> 
-//                 </tr>
-//               </table>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   } else {
+  //             />
+  //             <h1 className="p-2">Welcome to Go Fund Me Analytics!</h1>{" "}
+  //             <br></br>
+  //             <h3 className="p-2">
+  //               Please log in with your Google or Facebook account below to continue
+  //             </h3>
+  //             <br></br>
+  //             <table>
+  //               <tr>
+  //                 <td className='p-2'>
+  //                   <GoogleLogin
+  //                     clientId="264235252416-qu7uoto6sb62kc8hb7fffrg387o26e7e.apps.googleusercontent.com"
+  //                     buttonText="Login with Google"
+  //                     onSuccess={responseGoogle}
+  //                     onFailure={responseGoogle}
+  //                     cookiePolicy={"single_host_origin"}
+  //                     className="p-2"
+  //                   />
+  //                 </td>
+  //                 <td className='p-2'>
+  //                   <FacebookLogin
+  //                     appId="2498350440413934"
+  //                     autoLoad={true}
+  //                     fields="name,email,picture"
+  //                     callback={responseFacebook}
+  //                   />
+  //                 </td> 
+  //               </tr>
+  //             </table>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
+  // } else {
     return <App />;
   }
 //  }
